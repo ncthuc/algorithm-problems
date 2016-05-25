1ae2f1337dacd37a3ef86e0fb9515f8ebca11513
thucnguyen <gthuc.nguyen@gmail.com>
2016-05-25 23:02:54 +0700

    added

---
+++ src/land/study/CongTru1.java
@@ -0,0 +1,34 @@
+package land.study;
+
+/**
+ * Created by Thuc on 5/25/2016.
+ * CONG_TRU_1
+ */
+public class CongTru1 {
+    private int[] seeds = {27, 9, 3, 1, 0};
+
+    public void solve() {
+        for (int v = 40; v >=1; v--) {
+            String s = f(v, 0);
+            if (!s.endsWith("#")) {
+                System.out.println(v + " = " +  s.substring(3));
+            }
+        }
+    }
+
+    private String f(int v, int i) {
+        if (i >= seeds.length) return "#";
+
+        if (v==0) return "";
+
+        if (v == seeds[i]) return " + " + seeds[i];
+
+        String cong = f(v-seeds[i], i + 1);
+        if (!cong.endsWith("#")) return " + " + seeds[i] + cong;
+
+        String tru = f(v+seeds[i], i + 1);
+        if (!tru.endsWith("#")) return " - " + seeds[i] + tru;
+
+        return f(v, i+1);
+    }
+}
--- src/land/study/Main.java
+++ src/land/study/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MagicSquare magicSquare = new MagicSquare(3);
-        magicSquare.solve();
+//        MagicSquare magicSquare = new MagicSquare(3);
+//        magicSquare.solve();
+
+        CongTru1 congTru1 = new CongTru1();
+        congTru1.solve();
     }
 }
